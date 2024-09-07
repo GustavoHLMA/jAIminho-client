e0a959e15c981666b5fb8fb64d31643b52f335e1
Rennan Miranda <87147821+RennanJSM@users.noreply.github.com>
2024-09-06 22:28:56 -0300

    build

--- src/pages/chat/styles.ts
+++ src/pages/chat/styles.ts
@@ -1,7 +1,5 @@
 import { styled } from '@mui/system';
 import { Box, IconButton, TextField } from '@mui/material';
-import { Poppins } from '@next/font/google';
-
 
 export const Container = styled(Box)({
   display: 'flex',
@@ -19,7 +17,6 @@ export const NavContainer = styled(Box)`
   padding-right: 4.69vw;
 `;
 
-
 export const Navbar = styled(Box)({
   width: '100%',
   padding: '10px 20px',
@@ -31,27 +28,24 @@ export const Navbar = styled(Box)({
   marginBottom: '7rem'
 });
 
-
 export const Sidebar = styled(Box)({
-    width: '250px',              // Define a largura da sidebar
-    height: '100vh',             // Ocupa a altura total da viewport
-    backgroundColor: '#F9F9F9',      // Fundo branco para a sidebar
-    borderRight: '1px solid #ddd', // Borda para separar visualmente a sidebar
-    overflowY: 'auto',            // Adiciona a rolagem quando o conteúdo ultrapassar a altura
-    padding: '20px',
-    marginLeft: '18px',
-    scrollbarWidth: 'thin',       // Definir largura da scrollbar
-    scrollbarColor: '#ffcc00 transparent', // Cor personalizada da barra de rolagem
-    '&::-webkit-scrollbar': {
-      width: '8px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      backgroundColor: '#ffcc00', // Cor do scroll
-      borderRadius: '8px',
-    },
-  });
-  
-
+  width: '250px', // Define a largura da sidebar
+  height: '100vh', // Ocupa a altura total da viewport
+  backgroundColor: '#F9F9F9', // Fundo branco para a sidebar
+  borderRight: '1px solid #ddd', // Borda para separar visualmente a sidebar
+  overflowY: 'auto', // Adiciona a rolagem quando o conteúdo ultrapassar a altura
+  padding: '20px',
+  marginLeft: '18px',
+  scrollbarWidth: 'thin', // Definir largura da scrollbar
+  scrollbarColor: '#ffcc00 transparent', // Cor personalizada da barra de rolagem
+  '&::-webkit-scrollbar': {
+    width: '8px'
+  },
+  '&::-webkit-scrollbar-thumb': {
+    backgroundColor: '#ffcc00', // Cor do scroll
+    borderRadius: '8px'
+  }
+});
 
 export const ChatArea = styled(Box)({
   flex: 1,
@@ -61,63 +55,56 @@ export const ChatArea = styled(Box)({
   alignItems: 'center',
   padding: '20px',
   backgroundColor: '#ffffff',
-  height: '100vh',           
-  position: 'relative',
+  height: '100vh',
+  position: 'relative'
 });
 
-
 export const ChatInputArea = styled(Box)({
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 10px',          
-    backgroundColor: '#FFFBE3', 
-    borderRadius: '30px',       
-    border: '0.5px solid #FFDC02', 
-    width: '81%',
-    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', 
-    position: 'absolute',       
-    bottom: '20px',             
-    left: '50%',
-    transform: 'translateX(-50%)', 
-  });
-  
-  
+  display: 'flex',
+  alignItems: 'center',
+  padding: '0 10px',
+  backgroundColor: '#FFFBE3',
+  borderRadius: '30px',
+  border: '0.5px solid #FFDC02',
+  width: '81%',
+  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  position: 'absolute',
+  bottom: '20px',
+  left: '50%',
+  transform: 'translateX(-50%)'
+});
 
+export const InputField = styled(TextField)({
+  flex: 1,
+  backgroundColor: 'transparent',
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      display: 'none'
+    },
+    '&.Mui-focused fieldset': {
+      display: 'none'
+    }
+  },
+  '& input': {
+    padding: '10px'
+  }
+});
 
-  export const InputField = styled(TextField)({
-    flex: 1,
-    backgroundColor: 'transparent',  
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        display: 'none',              
-      },
-      '&.Mui-focused fieldset': {
-        display: 'none',              
-      },
-    },
-    '& input': {
-      padding: '10px',                
-    },
-  });
-  
-  
-  export const SendButton = styled(IconButton)({
-    backgroundColor: '#004785',      
-    color: '#FFD700',                
-    borderRadius: '50%',             
-    padding: '7px',                 
-    marginLeft: '10px',              
-    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', 
-    '&:hover': {
-      backgroundColor: '#003366',    
-    },
-  });
-  
+export const SendButton = styled(IconButton)({
+  backgroundColor: '#004785',
+  color: '#FFD700',
+  borderRadius: '50%',
+  padding: '7px',
+  marginLeft: '10px',
+  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  '&:hover': {
+    backgroundColor: '#003366'
+  }
+});
 
-  export const ClipButton = styled(IconButton)({
-    backgroundColor: 'transparent',
-    color: '#004785',
-    padding: '10px',
-    marginRight: '10px',
-  });
-  
+export const ClipButton = styled(IconButton)({
+  backgroundColor: 'transparent',
+  color: '#004785',
+  padding: '10px',
+  marginRight: '10px'
+});
